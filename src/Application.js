b41dea396c773d9b2edda030b2dd1461b0bafe68
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2023-06-08 21:19:50 -0300

    Simple beautiful window

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import Gtk from 'gi://Gtk'
+import Gtk from 'gi://Gtk?version=4.0'
 import GObject from 'gi://GObject'
 
 export const FbrApplication = GObject.registerClass(
@@ -10,9 +10,36 @@ export const FbrApplication = GObject.registerClass(
             console.log('Hello World!');
 
             const window = new Gtk.ApplicationWindow({ application: this, title: 'Welcome to Flatpak Builder' });
-            const label = new Gtk.Label({ label: 'Hello World!' });
+            const box = new Gtk.Box({ 
+                orientation: Gtk.Orientation.VERTICAL,
+                marginTop: 36,
+                marginBottom: 36,
+                marginStart: 36,
+                marginEnd: 36,
+                spacing: 18,
+                valign: Gtk.Align.CENTER,
+                halign: Gtk.Align.CENTER
+            });
+            
+            const label = new Gtk.Label({ 
+                label: 'Hello World!', 
+                wrap: true 
+            });
+            const image = new Gtk.Image({
+                iconName: 'system-file-manager-symbolic',
+                iconSize: Gtk.IconSize.LARGE
+            });
+            const button = new Gtk.Button({
+                label: 'Click me!',
+                halign: Gtk.Align.CENTER
+            })
 
-            window.set_child(label);
+            box.append(image);
+            box.append(label);
+            box.append(button);
+        
+            window.set_child(box);
+
             window.present();
         }
     }
